2983cec8e1796933579b6c0ec1d96cc3012859bf
twhassall <twhassall@gmail.com>
2026-06-25 01:04:22 +0100

    All slots start at the same time, but stop separately

--- scripts/slot_machine.gd
+++ scripts/slot_machine.gd
@@ -29,32 +29,58 @@ func _ready():
 func random_sprite_key():
 	return MusicLibrary.beat_sprites.keys().pick_random()
 	
-func spin_reel(reel, final_key):
-	player.stream = spinSound
-	player.play()
-	for i in range(20):
+func start_spin_reel(reel):
+	while reel.get_meta("spinning") == true:
 		reel.texture = MusicLibrary.beat_sprites[random_sprite_key()]
 		await get_tree().create_timer(0.05).timeout
+
+func stop_spin_reel(reel, final_key):
+	reel.set_meta("spinning", false)
 	reel.texture = MusicLibrary.beat_sprites[final_key]
-	player.stop()
+	if reel == reel4:
+		await get_tree().create_timer(0.1).timeout
+		player.stop()
 	
+func start_all_reels():
+	reel1.set_meta("spinning", true)
+	reel2.set_meta("spinning", true)
+	reel3.set_meta("spinning", true)
+	reel4.set_meta("spinning", true)
+
+	start_spin_reel(reel1)
+	start_spin_reel(reel2)
+	start_spin_reel(reel3)
+	start_spin_reel(reel4)
+
 func spin_all_reels(bar):
 	var key1 = MusicLibrary.beat_to_key(bar[0])
 	var key2 = MusicLibrary.beat_to_key(bar[1])
 	var key3 = MusicLibrary.beat_to_key(bar[2])
 	var key4 = MusicLibrary.beat_to_key(bar[3])
 
-	await spin_reel(reel1, key1)
+	start_all_reels()
+
+	await get_tree().create_timer(1.4).timeout
+	stop_spin_reel(reel1, key1)
 	await MusicLibrary.play_beat(bar[0])
-	await spin_reel(reel2, key2)
+
+	await get_tree().create_timer(0.4).timeout
+	stop_spin_reel(reel2, key2)
 	await MusicLibrary.play_beat(bar[1])
-	await spin_reel(reel3, key3)
+
+	await get_tree().create_timer(0.4).timeout
+	stop_spin_reel(reel3, key3)
 	await MusicLibrary.play_beat(bar[2])
-	await spin_reel(reel4, key4)
+
+	await get_tree().create_timer(0.4).timeout
+	stop_spin_reel(reel4, key4)
 	await MusicLibrary.play_beat(bar[3])
+	
 	render_bar(current_bar)
 
 func _on_spin_pressed():
+	player.stream = spinSound
+	player.play()
 	current_bar = MusicLibrary.generate_bar()
 	MusicLibrary.current_bar = current_bar
 	##visual
